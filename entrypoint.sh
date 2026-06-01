3c20c4630601a7d3007a40fe2aa80fe95cb98e2b
Kshitij Ayush <kshitij.ayush@nutanix.com>
2026-06-02 05:13:06 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Fix permissions on the mounted volume at runtime
+# Fix permissions as root
 mkdir -p /home/node/.n8n
-chown -R node:node /home/node/.n8n
-chmod -R 755 /home/node/.n8n
+chown -R 1000:1000 /home/node/.n8n
+chmod -R 777 /home/node/.n8n
 
-# Switch to node user and start n8n
-exec su-exec node n8n
+# Drop to node user and start n8n
+exec su node -s /bin/sh -c "n8n start"
